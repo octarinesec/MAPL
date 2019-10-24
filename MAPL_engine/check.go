b80eacd9517ea37324eccf08049c0cc1ec28e9e0
edodekel <edodekel@gamil.com>
2019-10-24 10:00:15 +0300

    debug

--- MAPL_engine/check.go
+++ MAPL_engine/check.go
@@ -390,6 +390,9 @@ func testOneCondition(c *Condition, message *MessageAttributes) bool {
 
 		result = true
 
+		log.Printf("valueToCompareStringArray=%+v",valueToCompareStringArray)
+
+
 		for _, valueToCompareString := range (valueToCompareStringArray) { // in case of an array we require all of the values to pass the condition
 			result_temp := false
 			L := len(valueToCompareString) - 1
